5166b94b517bd1daf3da494aa84c7c891fb1bc72
Abdullah Al Araf <125960268+iamaraf-git@users.noreply.github.com>
2023-08-05 12:29:51 +0600

    Create linked_list.cpp
    #Opposite_Order_Output

---
+++ C++/Data Structure /Linked List /linked_list.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+// Node structure for the linked list
+class Node
+{
+public :
+    int data;
+    Node* next;
+
+    Node(int value)
+    {
+        data = value;
+        next = nullptr;
+    }
+};
+
+// Linked list class
+class LinkedList
+{
+private:
+    Node* head;
+
+public:
+    LinkedList()
+    {
+        head = nullptr;
+    }
+
+    // Function to add a new node at the beginning of the list
+    void addNode(int value)
+    {
+        Node* newNode = new Node(value);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    // Function to display the linked list
+    void displayList()
+    {
+        Node* current = head;
+
+        while (current != nullptr)
+            {
+                cout << current->data << " ";
+                current = current->next;
+            }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    LinkedList myList;
+
+    // Add nodes to the linked list
+    myList.addNode(5);
+    myList.addNode(10);
+    myList.addNode(15);
+
+    // Display the linked list
+    cout << "Linked List: ";
+    myList.displayList();
+
+    return 0;
+}
