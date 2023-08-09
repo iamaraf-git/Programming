14483801ecdd716f0be969d209e6513bf392c1e5
Abdullah Al Araf <125960268+iamaraf-git@users.noreply.github.com>
2023-08-10 02:25:38 +0600

    Create triangular_printing.cpp

---
+++ C++/Problem /triangular_printing.cpp
@@ -0,0 +1,35 @@
+/*	
+for input 3;
+output 
+	 1 
+  2 2
+ 3 3 3
+*/
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    for(int i =n,  k =1 ; i>0 || k<=n ; i--, k++)
+    {
+        int j=0;
+
+        j=i;
+        while(j--)
+            cout << " ";
+
+        int m=0;
+        m=k;
+
+        while(m--)
+            cout << k<< " ";
+      
+        cout <<"\n";
+    }
+
+    return 0;
+}
